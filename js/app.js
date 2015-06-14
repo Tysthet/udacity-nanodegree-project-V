99df9476bb08f26430a893698c8e2475302bef49
Tysthet <baptiste.saugeron@gmail.com>
2015-06-14 18:37:39 +0200

    Fixed jQuery bug

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
-$.noConflict();
-jQuery(document).ready(function ($)  {
-	var model ={
+$(function () {
+	
+	var model = {
 		mapOptions: {
-			center: { lat: -34.397, lng: 150.644},
-			zoom: 8
+			center: { lat: 48.834536, lng: 2.317754},
+			zoom: 16
 		}
 	};
+	
 	var viewModel = {
 		init: function () {
-			var mapOptions = {
-				center: { lat: 48.834536, lng: 2.317754},
-				zoom: 16
-			};
-			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			var map = new google.maps.Map(document.getElementById('map-canvas'), model.mapOptions);
 		}
 	};
-	var seachView = {};
-	var markersView = {};
-
 	
-	// google.maps.event.addDomListener(window, 'load', initialize);
+	var searchView = {};
+	
+	var markersView = {};
+	
 	viewModel.init ();
-}) ();
+});
